46cfea1e15eb4041e19acda5c59634c8e3fbe68e
Gabriella Morgan <gabriellapelton@gmail.com>
2024-02-24 16:34:06 -0600

    rss feed crossposts to twitter

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import re
 import textwrap
 import time
 import feedparser
+import tweepy
 from dotenv import load_dotenv
 from atproto import Client, client_utils
 from html2text import html2text
 
 load_dotenv()
+RSS_URL = os.environ["RSS_URL"]
+BSKY_USER = os.environ["BSKY_USER"]
+BSKY_PASSWORD = os.environ["BSKY_PASSWORD"]
+API_KEY = os.environ["API_KEY"]
+API_SECRET = os.environ["API_SECRET"]
+BEARER_TOKEN = os.environ["BEARER_TOKEN"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 
 
 def format_rss_to_text(entry, character_limit, hyperlink_max_length):
@@ -41,9 +50,9 @@ def post_to_bluesky(entry):
     if len(content_with_link) == 2:
         tb.text(content_with_link[0])
         tb.link(content_with_link[1], f'https://{content_with_link[1]}')
-        client.send_post(tb)
+        bsky_client.send_post(tb)
     else:
-        client.send_post(content)
+        bsky_client.send_post(content)
     return
 
 
@@ -51,7 +60,9 @@ def post_to_mastodon():
     return
 
 
-def post_to_twitter():
+def post_to_twitter(entry):
+    content = format_rss_to_text(entry, 280, 23)
+    twitter_client.create_tweet(text=content)
     return
 
 
@@ -59,17 +70,24 @@ if __name__ == "__main__":
     start_time = int(time.time())
     url = os.environ["RSS_URL"]
     old_entries = []
-    client = Client(base_url='https://bsky.social')
-    [print(os.environ['BSKY_USER'])]
-    client.login(os.environ['BSKY_USER'], os.environ['BSKY_PASSWORD'])
+    twitter_client = tweepy.Client(
+        consumer_key=API_KEY,
+        consumer_secret=API_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET
+    )
+
+    bsky_client = Client(base_url='https://bsky.social')
+    bsky_client.login(os.environ['BSKY_USER'], os.environ['BSKY_PASSWORD'])
     while True:
         feed = feedparser.parse(url)
         for entry in feed.entries:
             if entry.id in old_entries:
-                continue
+                pass
             elif (calendar.timegm(feed.entries[0].published_parsed) < start_time):
                 old_entries.append(entry.id)
             else:
                 post_to_bluesky(entry)
+                post_to_twitter(entry)
                 old_entries.append(entry.id)
-        time.sleep(10)
+        time.sleep(300)
